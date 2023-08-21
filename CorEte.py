96cae8f221ddaaf7cbf63ad769d31aabc920c4a8
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-08-21 17:36:25 +0300

    Update ETElib.py

--- CorEte.py
+++ CorEte.py
@@ -5,16 +5,21 @@ from ete3 import Tree
 from ete3 import TreeStyle, NodeStyle, TreeNode, TextFace
 import re
 import random
+import os
 
 ## this is a collection of ETE3 functions for custom use ##
 
-def midpoint(input):
+## General functions for midpoint rooting, node collapse based on bootstrap values and polytomy resolving ##
+def midpoint(input):    
     tree = Tree(input, format = 1)
+    
+    ## get midpoint root of tree ##
+    midpoint = tree.get_midpoint_outgroup()
 
-    midpoint = tree.get_midpoint_outgroup() ## get midpoint root of tree ##
-
-    tree.set_outgroup(midpoint) ## set midpoint root as outgroup ##
+    ## set midpoint root as outgroup ##
+    tree.set_outgroup(midpoint)
     tree.write(format=1, outfile=input+".midpoint_rooted")
+    return
 
 
 def bootstrap_collapse(tree, threshold):
@@ -25,53 +30,113 @@ def bootstrap_collapse(tree, threshold):
                 else:
                         return node
 
+                   
+def resolve_polytomies(input):   
+    tree = Tree(input, format = 1)   
+    tree.resolve_polytomy(recursive=True) ## resolve polytomies in tree ##
+    tree.write(format=1, outfile=input+".resolved_polytomies")
+    return
+
+
+
+## Tree visualization functions ##
+
+def color_node(node): ## function to color node and all descendants ##
+    node.img_style["fgcolor"] = node_color
+    for child in node.children:
+        color_node(child)
+
+
+## main visualization function for leaf coloring and bootstrap support ##
 def visualize_tree(tree, reference, layout = "c"):
-    t=Tree(tree)
-    ts = TreeStyle()
-    ts.show_leaf_name = False
-    ts.mode = layout
-    species_colors = {}
+        t=Tree(tree)
+        ts = TreeStyle()
+        ts.show_leaf_name = False
+        ts.mode = layout
 
-    for leaf in t.iter_leaves(): ## Assign a unique color to each species ##
-        species = re.sub("_.*","", leaf.name)
-            
-        if species not in species_colors:
-            color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
-            if color != "#000000": ## exclude black color, keep it only for reference species ##
-                species_colors[species] = color
+        species_colors = {}
 
-    thresholds = {
-        50: "grey",
-        75: "darkgrey",
-        100: "black"
-    }
+        ## Assign a unique color to each species ##
+        for leaf in t.iter_leaves():
+                if not re.search (reference, leaf.name ):
+                        species = re.sub("_.*","", leaf.name)
 
-    for node in t.traverse():
-        nstyle=NodeStyle()
-        nstyle["size"] = 0
-        node.set_style(nstyle)
+                if species not in species_colors:
+                        color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+                        species_colors[species] = color
 
-        if node.is_leaf(): ## Species associated with different colors ##
-            if re.search ( reference, node.name ):
-                color_n = "black"
-            else:
-                species_n = re.sub ("_.*","",node.name)
-                color_n = species_colors[species_n]
-                
-            species_face = TextFace(node.name,fgcolor=color_n, fsize=10,ftype="Arial")
-            node.add_face(species_face, column=1, position='branch-right')
+        thresholds = {
+                50: "grey",
+                75: "darkgrey",
+                100: "black"
+        }
 
-        else: ## Circles representing bootstrap value ##
-            for threshold, col in thresholds.items():
-                if node.support <= 50:
-                    color_b = "lightgrey"
-                elif node.support >= threshold:
-                    color_b = col
-            if color_b:
-                node_style=NodeStyle()
-                node_style["fgcolor"] = color_b
-                node_style["size"] = 5
-                node.set_style(node_style)
-                
-    t.render(tree+".svg", w=500, units="mm", tree_style=ts)
-    t.show(tree_style=ts)
+        for node in t.traverse():
+                nstyle=NodeStyle()
+                nstyle["size"] = 0
+                node.set_style(nstyle)
+
+                ## Species associated with different colors ##
+                if node.is_leaf():
+                        if re.search(reference,node.name):
+                                reference_face = TextFace(node.name,fgcolor="black", fsize=10,ftype="Arial")
+                                node.add_face(reference_face, column=0, position='branch-right')
+
+                        else:
+                                species_n=re.sub("_.*","",node.name)
+                                color_n = species_colors[species_n]
+                                species_face = TextFace(node.name,fgcolor=color_n, fsize=10,ftype="Arial")
+                                node.add_face(species_face, column=1, position='branch-right')
+
+                ## Circles representing bootstrap value ##
+                else:
+                        for threshold, col in thresholds.items():
+                                if node.support <= 50:
+                                        color_b = "lightgrey"
+                                elif node.support >= threshold:
+                                        color_b = col
+
+                        if color_b:
+                                node_style=NodeStyle()
+                                node_style["fgcolor"] = color_b
+                                node_style["size"] = 5
+                                node.set_style(node_style)
+
+
+        # save tree to svg and show
+        t.render(tree+".svg", w=500, units="mm", tree_style=ts)
+        t.show(tree_style=ts)
+
+
+def visualize_subtrees ( file ):
+    if file.endswith(".nwk"):
+        visualize_tree ( file, layout= "c" )
+
+## Replace geneids in newick with names from a gene ID list ##
+
+def fasta_names ( fasta ):
+    gene_names = {}
+    fasta=fasta.strip ("\n")
+    id=re.sub(">|_.*","",fasta)
+    name=re.sub(">|/","",fasta)
+    gene_names[id]=name
+    return gene_names
+
+
+def sub_names_nwk(names, newick):
+    gene_names = {}
+    for file in open(names, "r").readlines():
+        fasta = file.strip()
+        gene_names.update(fasta_names(fasta))
+
+    with open(newick+".names", "w") as file:
+
+        new_nwk=""
+        for file2 in open ( newick, "r" ).readlines():
+            nwk = file2.strip()
+
+            for ids in gene_names.keys():
+                nwk = re.sub(ids,gene_names[ids],nwk)
+                new_nwk = nwk
+
+        return file.write(new_nwk)
