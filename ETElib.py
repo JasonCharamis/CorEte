137ce507d276d9dccc511fe84af188b6547a864f
“JasonCharamis” <jason.charamis@gmail.com>
2023-10-27 10:19:28 +0300

    Changes

--- ETElib.py
+++ ETElib.py
@@ -1,15 +1,71 @@
-import numpy as np
-import pandas as pd
 import seaborn as sns
-from ete3 import Tree
-from ete3 import TreeStyle, NodeStyle, TreeNode, TextFace
-import re
-import random
-import os
+from ete3 import Tree, TreeStyle, NodeStyle, TextFace
+import re, random, os
+import argparse
 
-## this is a collection of ETE3 functions for custom use ##
+## Collection of functions to manipulate and visualize phylogenetic trees using the ETE3 toolkit ##
 
-## General functions for midpoint rooting, node collapse based on bootstrap values and polytomy resolving ##
+def parseCmdArguments():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Library for efficiently manipulating gff3 files.')
+    parser.add_argument('-t','--tree', type=str, help='Nwk tree input file.')
+    parser.add_argument('-m','--midpoint', action="store_true",help='Midpoint root tree.')
+    parser.add_argument('--collapse', action="store_true",help='Collapse nodes based on bootstrap support.')
+    parser.add_argument('--cutoff', type=str,help='Bootstrap support cutoff for collapsing nodes.')
+    parser.add_argument('-r','--resolve', action="store_true",help='Resolve polytomies.')
+    parser.add_argument('-v','--visualize', action="store_true",help='Visualize tree.')
+    parser.add_argument('-l','--layout', type=str,help='Layout to visualize tree. Default: circular')
+    parser.add_argument('-c','--count', action="store_true",help='Count leaves')
+    parser.add_argument('-n','--names', action="store_true",help='Option to replace names in nwk')
+    parser.add_argument('-nf','--names_file', type = str ,help='File with names to replace. Default is second column in tab-separated format.')
+    args = parser.parse_args()
+
+    if not any(vars(args).values()):
+        parser.print_help()
+        print("Error: No arguments provided.")
+
+    return parser.parse_args()
+
+## Implementation ##
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Library for efficiently manipulating newick files and trees.')
+    args = parseCmdArguments()
+    
+    if args.tree:
+
+        inp = re.sub (".nwk$","",args.tree)
+        
+        if args.midpoint:
+            midpoint(args.tree)
+
+        elif args.collapse:
+            if args.cutoff:
+                bootstrap_collapse(args.tree,args.cutoff)
+            else:
+                print ( "Please provide a bootstrap cutoff.")
+            
+        elif args.visualize:
+            visualize_tree(args.tree, layout=args.layout if args.layout else 'c' )
+
+        elif args.resolve:
+            resolve_polytomies(args.tree)
+
+        elif args.names:
+            if args.names_file:
+                print ( sub_names_nwk(args.tree, args.names_file ) )
+            else:
+                print ( "Please provide a list with gene names to replace.")
+
+        elif args.count:
+            print ( count_leaves(args.tree) )
+            
+    else:
+        print ("Please provide a nwk file as input.")
+
+
+## Midpoint rooting ##
 def midpoint(input):    
     tree = Tree(input, format = 1)
     
@@ -18,19 +74,19 @@ def midpoint(input):
 
     ## set midpoint root as outgroup ##
     tree.set_outgroup(midpoint)
-    tree.write(format=1, outfile=input+".midpoint_rooted")
+    tree.write(format=1, outfile=input+".midpointed")
     return
 
 
-def bootstrap_collapse(tree, threshold):
-        t=Tree(tree)
-        for node in t.traverse():
-                if node.support < threshold:
-                        return node.delete()
-                else:
-                        return node
+def bootstrap_collapse(tree, threshold=50):
+    t=Tree(tree)
+    for node in t.traverse():
+        if node.support < threshold:
+            return node.delete()
+        else:
+            return node
 
-                   
+
 def resolve_polytomies(input):   
     tree = Tree(input, format = 1)   
     tree.resolve_polytomy(recursive=True) ## resolve polytomies in tree ##
@@ -38,79 +94,130 @@ def resolve_polytomies(input):
     return
 
 
+def count_leaves ( tree ):
+    nleaves = []
 
-## Tree visualization functions ##
+    t = Tree(tree)
+    
+    for leaf in t.iter_leaves():
+        nleaves.append(leaf)
+
+    counts = len(nleaves)
+    
+    return counts
+        
 
 def color_node(node): ## function to color node and all descendants ##
     node.img_style["fgcolor"] = node_color
     for child in node.children:
         color_node(child)
 
+## Main tree visualization function ##
+def visualize_tree(tree, layout = "c"):
+    t=Tree(tree)
 
-## main visualization function for leaf coloring and bootstrap support ##
-def visualize_tree(tree, reference, layout = "c"):
-        t=Tree(tree)
-        ts = TreeStyle()
-        ts.show_leaf_name = False
-        ts.mode = layout
+    ts = TreeStyle()
+    ts.show_leaf_name = False
+    ts.mode = layout
 
-        species_colors = {}
+    species_colors = {}
 
-        ## Assign a unique color to each species ##
-        for leaf in t.iter_leaves():
-                if not re.search (reference, leaf.name ):
-                        species = re.sub("_.*","", leaf.name)
+    for leaf in t.iter_leaves(): ## Assign a unique color to each species ##
+        leaf.name=re.sub ( "^g","Llongipalpis_g", leaf.name )
+        leaf.name = re.sub ( "\..*", "", leaf.name )
 
-                if species not in species_colors:
-                        color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
-                        species_colors[species] = color
+        if re.search ( "_", leaf.name ): ## If _ is present, get the prefix as species name
+            species = re.sub("_.*","", leaf.name)
 
-        thresholds = {
-                50: "grey",
-                75: "darkgrey",
-                100: "black"
-        }
+        else: ## If no _ is found, get the species names from the first four letters of the gene ID
+            species = leaf.name[0:3]
+            
+        if species not in species_colors:
+            color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+            if color != "#000000": ## keep black color for special identifiers ##
+                species_colors[species] = color
 
-        for node in t.traverse():
-                nstyle=NodeStyle()
-                nstyle["size"] = 0
-                node.set_style(nstyle)
+    thresholds = {
+        50: "grey",
+        75: "darkgrey",
+        100: "black"
+    }
 
-                ## Species associated with different colors ##
-                if node.is_leaf():
-                        if re.search(reference,node.name):
-                                reference_face = TextFace(node.name,fgcolor="black", fsize=10,ftype="Arial")
-                                node.add_face(reference_face, column=0, position='branch-right')
+    for node in t.traverse():
+        nstyle=NodeStyle()
+        nstyle["size"] = 0
+        node.set_style(nstyle)
 
-                        else:
-                                species_n=re.sub("_.*","",node.name)
-                                color_n = species_colors[species_n]
-                                species_face = TextFace(node.name,fgcolor=color_n, fsize=10,ftype="Arial")
-                                node.add_face(species_face, column=1, position='branch-right')
+        if node.is_leaf(): ## if nodes are leaves, get color name based on species ##   
+            if re.search ( "_", node.name ): ## If _ is present, get the prefix as species name
+                species_n = re.sub("_.*","", node.name)
+                
+            else: ## If no _ is found, get the species names from the first four letters of the gene ID
+                species_n = node.name[0:3]
+                
+            color_n = species_colors[species_n]               
+            species_face = TextFace(node.name,fgcolor=color_n, fsize=500,ftype="Arial")
+            node.add_face(species_face, column=1, position='branch-right')
 
-                ## Circles representing bootstrap value ##
-                else:
-                        for threshold, col in thresholds.items():
-                                if node.support <= 50:
-                                        color_b = "lightgrey"
-                                elif node.support >= threshold:
-                                        color_b = col
+        if node.support < 1: ## convert all bootstrap values to hundreds
+            node.support = node.support*100
 
-                        if color_b:
-                                node_style=NodeStyle()
-                                node_style["fgcolor"] = color_b
-                                node_style["size"] = 5
-                                node.set_style(node_style)
-
-
-        # save tree to svg and show
-        t.render(tree+".svg", w=500, units="mm", tree_style=ts)
-        t.show(tree_style=ts)
+        for threshold, col in thresholds.items(): ## bootstrap values in internal nodes represented as circles ##
+            if node.support <= 50:
+                color_b = "lightgrey"
+            elif node.support >= threshold:
+                color_b = col
+                
+        if color_b:
+            node_style=NodeStyle()
+            node_style["fgcolor"] = color_b
+            node_style["size"] = 500
+            node.set_style(node_style)
 
 
-def visualize_subtrees ( file ):
-    if file.endswith(".nwk"):
-        visualize_tree ( file, layout= "c" )
+    t.render(tree+".svg", w=5000, units="mm", tree_style=ts)
+#    t.show(tree_style=ts)
+
+    return
+
+
+## Leaf counting functions ##
+def count_descendant_leaves ( tree, node ):
+    t=Tree(tree)
+    descendant_leaves = []
+
+    for node in t.traverse ("preorder"):
+        descendant_leaves.append ( node.get_leaf_names() )
+        
+    print ("You have", len(descendant_leaves), "leaves" )
+    return
+
+
+def count_leaves_by_taxon ( tree, taxon_ID ):
+    t=Tree(tree)
+    descendant_leaves = []
+
+    for node in t.traverse ("preorder"):
+        if node.is_leaf():
+            if re.search ( taxon_ID, node.name ):
+                descendant_leaves.append ( node.get_leaf_names() )
+                
+    print ("You have", len(descendant_leaves), "leaves for", taxon_ID )
+    return
+
+
+def count_descendant_leaves_by_taxon ( tree, node, taxon_ID ):
+    t=Tree(tree)
+    descendant_leaves = []
+
+    for node in t.traverse ("preorder"):
+        if node.is_leaf():
+            if re.search ( taxon_ID, node.name ):
+                descendant_leaves.append ( node.get_leaf_names() )
+
+    print ("You have", len(descendant_leaves), "descendant leaves for", taxon_ID )
+    return
+
 
 ## Replace geneids in newick with names from a gene ID list ##
 
@@ -123,20 +230,21 @@ def fasta_names ( fasta ):
     return gene_names
 
 
-def sub_names_nwk(names, newick):
-    gene_names = {}
-    for file in open(names, "r").readlines():
-        fasta = file.strip()
-        gene_names.update(fasta_names(fasta))
+def get_newick(node):   
+    if node.is_leaf():
+        return f"{node.name}:{node.dist}"
+    
+    else:
+        children_newick = ",".join([get_newick(child) for child in node.children])
 
-    with open(newick+".names", "w") as file:
+        if hasattr(node, "support"):
+            return f"({children_newick}){node.name}:{node.dist}[&support={node.support}]"
+        else:
+            return f"({children_newick}){node.name}:{node.dist}"
 
-        new_nwk=""
-        for file2 in open ( newick, "r" ).readlines():
-            nwk = file2.strip()
+    return f"({children_newick}){node.name}:{node.dist}"
 
-            for ids in gene_names.keys():
-                nwk = re.sub(ids,gene_names[ids],nwk)
-                new_nwk = nwk
 
-        return file.write(new_nwk)
+if __name__ == "__main__":
+    main()
+
